1e4e93b548bcc64ed5e7f9407b95ea9edd64a49e
amserhii11 <amelin.serhii@gmail.com>
2019-10-01 16:33:10 +0300

    Updates

---
+++ precache-manifest.bd8bff2fe845af2ccf7e97554efc0eeb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68a9846d098543f5caf9248906b51836",
+    "url": "/react_tabs-with-router/index.html"
+  },
+  {
+    "revision": "05e85bf07cb170be7a65",
+    "url": "/react_tabs-with-router/static/css/main.65938976.chunk.css"
+  },
+  {
+    "revision": "02243efc06f380ddf1d1",
+    "url": "/react_tabs-with-router/static/js/2.9f9a8f39.chunk.js"
+  },
+  {
+    "revision": "05e85bf07cb170be7a65",
+    "url": "/react_tabs-with-router/static/js/main.7fdb387e.chunk.js"
+  },
+  {
+    "revision": "da3ca6fceb301548d22d",
+    "url": "/react_tabs-with-router/static/js/runtime~main.5858125e.js"
+  }
+]);
